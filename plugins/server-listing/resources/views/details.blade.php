766a7f9905fab56f459e40b6d6f8d10bd7ba4445
Mahfuz Ahmed Zisan <mahfuz.ahmed.zisan@gmail.com>
2025-08-06 16:39:10 +0600

    Update details.blade.php

--- plugins/server-listing/resources/views/details.blade.php
+++ plugins/server-listing/resources/views/details.blade.php
@@ -423,7 +423,7 @@
                 <li class="breadcrumb-item">
                     <a href="#" class="text-decoration-none title-color">Minecraft Servers List</a>
                 </li>
-                <li class="breadcrumb-item active" aria-current="page">Mahfuz Ahmed Zisan</li>
+                <li class="breadcrumb-item active" aria-current="page">Mahfuz Ahmed Zisan </li>
             </ol>
         </div>
 
