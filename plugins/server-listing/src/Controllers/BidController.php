3dfc85cf60bb81fe119f9da432ae23a3ee21cfaa
Md Ahoshan Habib <150496607+ahoshan20@users.noreply.github.com>
2025-08-18 17:15:16 +0600

    Update BidController.php

--- plugins/server-listing/src/Controllers/BidController.php
+++ plugins/server-listing/src/Controllers/BidController.php
@@ -1,38 +1,13 @@
 <?php
 
 use Azuriom\Http\Controllers\Controller;
-use Azuriom\Plugin\ServerListing\Models\Bid;
-use Azuriom\Plugin\ServerListing\Models\ServerListing;
-use Azuriom\Plugin\ServerListing\Requests\BidRequest;
-use Azuriom\Plugin\ServerListing\Services\BidService;
-use Illuminate\Http\Request;
-use Throwable;
 
 class BidController extends Controller
 {
     protected BidService $bidService;
-    public function __construct(public BidService $bidService)
+    public function __construct(BidService $bidService)
     {
         $this->middleware(['auth:web', 'verified']);
-    }
-
-    public function store(ServerListing $serverListing, BidRequest $request)
-    {
-        try {
-            $this->bidService->store($serverListing, $request->validated());
-            return back()->with('success', 'Bid placed successfully');
-        } catch (Throwable $e) {
-            return back()->with('error', $e->getMessage());
-        }
-    }
-
-    public function destroy(ServerListing $serverListing, Bid $bid)
-    {
-        try {
-            $this->bidService->destroy($serverListing, $bid);
-            return back()->with('success', 'Bid removed successfully');
-        } catch (Throwable $e) {
-            return back()->with('error', $e->getMessage());
-        }
+        $this->bidService = $bidService;
     }
 }
