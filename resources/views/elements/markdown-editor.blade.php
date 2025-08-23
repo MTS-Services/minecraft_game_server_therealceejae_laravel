661fd3785229e1aafaf4f804475f48aa40ba467f
Md Shariful Islam <116838065+devshariful99@users.noreply.github.com>
2025-08-23 17:49:50 +0600

    Update markdown-editor.blade.php

--- resources/views/elements/markdown-editor.blade.php
+++ resources/views/elements/markdown-editor.blade.php
@@ -84,55 +84,50 @@
             spellChecker: false,
             status: ['upload-image'],
 
-            showIcons: ['strikethrough', 'code', '{{ isset($imagesUploadUrl) ? 'upload-image' : 'image' }}',
-                'table', 'horizontal-rule', 'undo', 'redo'
-            ],
+            showIcons: ['strikethrough', 'code', '{{ isset($imagesUploadUrl) ? 'upload-image' : 'image' }}', 'table', 'horizontal-rule', 'undo', 'redo'],
             iconClassMap: easyMdeIcons,
             autoDownloadFontAwesome: false,
 
             @isset($autosaveId)
-                autosave: {
-                    enabled: true,
-                    uniqueId: '{{ $autosaveId }}',
-                },
+            autosave: {
+                enabled: true,
+                uniqueId: '{{ $autosaveId }}',
+            },
             @endisset
 
             @isset($imagesUploadUrl)
-                hideIcons: ['image'],
-                uploadImage: true,
-                imageAccept: '.jpg,.jpeg,.jpe,.png,.gif,.bmp,.svg,.webp',
-                imageUploadFunction: function(file, onSuccess, onError) {
-                    if (file.size > easyMde.options.imageMaxSize) {
-                        onError(easyMde.options.errorMessages.fileTooLarge);
+            hideIcons: ['image'],
+            uploadImage: true,
+            imageAccept: '.jpg,.jpeg,.jpe,.png,.gif,.bmp,.svg,.webp',
+            imageUploadFunction: function (file, onSuccess, onError) {
+                if (file.size > easyMde.options.imageMaxSize) {
+                    onError(easyMde.options.errorMessages.fileTooLarge);
+                    return;
+                }
+
+                const formData = new FormData();
+                formData.append('file', file);
+
+                axios.post('{{ $imagesUploadUrl }}', formData, {
+                    onUploadProgress: function (progressEvent) {
+                        if (progressEvent.lengthComputable) {
+                            const progress = Math.round((progressEvent.loaded * 100) / progressEvent.total) + '';
+                            easyMde.updateStatusBar('upload-image', easyMde.options.imageTexts.sbProgress.replace('#file_name#', file.name).replace('#progress#', progress));
+                        }
+                    }
+                }).then(function (response) {
+                    onSuccess(response.data.location);
+                }).catch(function (error) {
+                    if (error.response) {
+                        onError(error.response.data.message);
                         return;
                     }
 
-                    const formData = new FormData();
-                    formData.append('file', file);
+                    onError(easyMde.options.errorMessages.importError);
 
-                    axios.post('{{ $imagesUploadUrl }}', formData, {
-                        onUploadProgress: function(progressEvent) {
-                            if (progressEvent.lengthComputable) {
-                                const progress = Math.round((progressEvent.loaded * 100) /
-                                    progressEvent.total) + '';
-                                easyMde.updateStatusBar('upload-image', easyMde.options.imageTexts
-                                    .sbProgress.replace('#file_name#', file.name).replace(
-                                        '#progress#', progress));
-                            }
-                        }
-                    }).then(function(response) {
-                        onSuccess(response.data.location);
-                    }).catch(function(error) {
-                        if (error.response) {
-                            onError(error.response.data.message);
-                            return;
-                        }
-
-                        onError(easyMde.options.errorMessages.importError);
-
-                        console.error('Image upload error: ' + error);
-                    });
-                },
+                    console.error('Image upload error: ' + error);
+                });
+            },
             @endisset
 
             imageTexts: {
